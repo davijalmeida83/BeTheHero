41478a14fdef483cb7767f0fc5782beebcec1f6e
Davi Almeida <davij_almeida@hotmail.com>
2022-09-06 13:02:09 -0300

    Update knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -43,9 +43,11 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: 'ec2-44-205-63-142.compute-1.amazonaws.com',
+      port: '5432',
+      database: 'd66s9l12bu6ih7',
+      user:     'vvttzruolzeiro',
+      password: '92da1617921ee100cac926374f0b0f3a70dd3f4fda9d69e0e03c04c7253e73d0'
     },
     pool: {
       min: 2,
